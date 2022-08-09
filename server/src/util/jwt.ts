4262d2eb69cce6d5f3169039ddb162c545555e38
1807869 <yysun2000@thehyundai.com>
2022-08-09 19:50:39 +0900

    test

--- server/src/util/jwt.ts
+++ server/src/util/jwt.ts
@@ -1,9 +1,9 @@
 import { sign, verify } from 'jsonwebtoken';
 
-export function generateAccessToken(userId : string) {
+export function generateAccessToken(userId : any) {
   return sign({ userId }, process.env.JWT_ACCESS_TOKEN_SECRET!, { expiresIn: '15m' });
 }
 
-export function verifyAccessToken(token: string) {
+export function verifyAccessToken(token: any) {
   return verify(token, process.env.JWT_ACCESS_TOKEN_SECRET!);
 }
