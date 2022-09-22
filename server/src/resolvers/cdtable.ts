69180472bae45a468ff78346aef6674aa39d7ad1
0sunman <youyoungsun2000@gmail.com>
2022-09-22 15:45:20 +0900

    dbUpdate

--- server/src/resolvers/cdtable.ts
+++ server/src/resolvers/cdtable.ts
@@ -1,35 +1,48 @@
 import {Resolver} from './types'
 // import {writeDB, DBField, modifyDB} from './../jsondb'
-import { v4 } from 'uuid';
 
 // const setJSON = (data:any) => writeDB(DBField.CONTENTS, data);
 // const modifyJSON = (id:string, title:string, path:string, selector:string, data:any) => modifyDB(DBField.CONTENTS, id, title, path,selector, data);
+/*
+    type Relation {
+        id:Int!,
+        componentid:String,
+        documentid:Int
+    }
+    extend type Query{
+        relations:[Relation]
+        relation(Relation:Int!): [Relation]
+    }
+    extend type Mutation{
+        addRelation(
+            componentid:String,
+            documentid:Int
+            ):[Relation]
+        removeRelation(id:Int!):[Relation]
+    }
+
+*/
+
 const cdRelationResolver:Resolver = {
         Query:{
-            contents:async (parent,args,{data})=>{
-                const result = await data('componenttool').select();
+            relations:async (parent,args,{data})=>{
+                const result = await data('cdtable').select();
                 return result;
             },
-            content:async (parent,{id},{data})=>{
-                const result = await data('componenttool').select().where("id",id);
+            relation:async (parent,{id},{data})=>{
+                const result = await data('cdtable').select().where("id",id);
                 return result;
             }
         },
         Mutation:{
-            addContent:async (parent,{title,path,selector,content,imgUrl},{data},info)=>{
-                await data('componenttool').insert({id:v4(),title,path,selector,content,imgUrl});
-                const result = await data('componenttool').select();
-                console.log(result);
+            addRelation:async (parent,{componentid, documentid},{data},info)=>{
+                await data('cdtable').insert({componentid, documentid});
+                const result = await data('cdtable').select();
                 return result;
             },
-            modifyContent:async (parent,{id,title,path,selector,content,imgUrl},{data},info)=>{
-                await data('componenttool').where("id",id).update({title,path,selector,content,imgUrl})
-                const result = await data('componenttool').select();
-                return result;
-            },
-            removeContent:async (parent,{id},{data},info)=>{
-                await data('componenttool').where("id",id).del();
-                const result = await data('componenttool').select();
+            removeRelation:async (parent,{id},{data},info)=>{
+                await data('cdtable').where("id",id).del();
+                const result = await data('cdtable').select();
                 return result;
             }
         }
--- server/src/resolvers/document.ts
+++ server/src/resolvers/document.ts
@@ -1,10 +1,19 @@
 import {Resolver} from './types'
 // import {writeDB, DBField, modifyDB} from './../jsondb'
-import { v4 } from 'uuid';
 
 // const setJSON = (data:any) => writeDB(DBField.CONTENTS, data);
 // const modifyJSON = (id:string, title:string, path:string, selector:string, data:any) => modifyDB(DBField.CONTENTS, id, title, path,selector, data);
 /*
+type Document {
+        id:Int!,
+        author:String,
+        content:String,
+        title:String,
+        imgUrl:String
+        selector:String,
+        createdAt:String,
+    }
+
 extend type Query{
         documents:[Document]
         document(id:Int!): [Document]
@@ -32,30 +41,34 @@ extend type Query{
 
 const documentResolver:Resolver = {
         Query:{
-            contents:async (parent,args,{data})=>{
-                const result = await data('componenttool').select();
+            documents:async (parent,args,{data})=>{
+                const result = await data('documents').select();
                 return result;
             },
-            content:async (parent,{id},{data})=>{
-                const result = await data('componenttool').select().where("id",id);
+            document:async (parent,{id},{data})=>{
+                const result = await data('documents').select().where("id",id);
+                return result;
+            },
+            documentslike:async (parent,{title},{data})=>{
+                const result = await data('documents').select().where("title","like",`%${title}%`);
                 return result;
             }
         },
         Mutation:{
-            addContent:async (parent,{title,path,selector,content,imgUrl},{data},info)=>{
-                await data('componenttool').insert({id:v4(),title,path,selector,content,imgUrl});
-                const result = await data('componenttool').select();
+            addDocument:async (parent,{author, content, title, imgUrl, selector},{data},info)=>{
+                await data('documents').insert({author, content, title, imgUrl, selector});
+                const result = await data('documents').select();
                 console.log(result);
                 return result;
             },
-            modifyContent:async (parent,{id,title,path,selector,content,imgUrl},{data},info)=>{
-                await data('componenttool').where("id",id).update({title,path,selector,content,imgUrl})
-                const result = await data('componenttool').select();
+            modifyDocument:async (parent,{id, author, content, title, imgUrl, selector},{data},info)=>{
+                await data('documents').where("id",id).update({author, content, title, imgUrl, selector})
+                const result = await data('documents').select();
                 return result;
             },
-            removeContent:async (parent,{id},{data},info)=>{
-                await data('componenttool').where("id",id).del();
-                const result = await data('componenttool').select();
+            removeDocument:async (parent,{id},{data},info)=>{
+                await data('documents').where("id",id).del();
+                const result = await data('documents').select();
                 return result;
             }
         }
