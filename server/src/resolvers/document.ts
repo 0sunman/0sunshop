0715e5845ee158dca8f4334f9f8494a47d858db8
0sunman <youyoungsun2000@gmail.com>
2022-09-25 21:29:41 +0900

    testo

--- server/src/resolvers/document.ts
+++ server/src/resolvers/document.ts
@@ -58,6 +58,7 @@ const documentResolver:Resolver = {
             addDocument:async (parent,{author, content, title, imgUrl, selector, path},{data},info)=>{
                 await data('documents').insert({author, content, title, imgUrl, selector, path});
                 const result = await data('documents').select().limit(1).orderBy('id', 'desc');
+                console.log(result);
                 return Number(result[0].id);
             },
             modifyDocument:async (parent,{id, author, content, title, imgUrl, selector, path},{data},info)=>{
