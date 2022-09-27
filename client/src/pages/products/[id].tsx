3f8b9282848d214ade6673323510240a92bfd5ee
1807869 <yysun2000@thehyundai.com>
2022-09-27 10:57:38 +0900

    ttest

--- client/src/pages/products/[id].tsx
+++ client/src/pages/products/[id].tsx
@@ -5,11 +5,22 @@ import { ProductCustom } from "../../types";
 import ProductDetailPage from "../../components/product/ProductDetail";
 
 import {GET_PRODUCT} from "../../graphql/products";
+import { ShopisLoading } from "../../recoils/layout";
+import { useRecoilState } from "recoil";
+import { useEffect } from "react";
 
 const ProductDetail = ()=> {
     const {id} = useParams();
-    const { data } = useQuery<{product:ProductCustom}>([QueryKeys.PRODUCTS, id], ()=> graphqlFetcher(GET_PRODUCT,{id}))
+    const { data, isFetching } = useQuery<{product:ProductCustom}>([QueryKeys.PRODUCTS, id], ()=> graphqlFetcher(GET_PRODUCT,{id}))
     // 내부의 속성의 타입 = {attr:TYPE}
+    const [isLoadingSite,setIsLoading] = useRecoilState(ShopisLoading);
+    useEffect(()=>{
+        if(isFetching){
+            setIsLoading(true);
+        }else{
+            setIsLoading(false);
+        }
+    },[isFetching])
     if(!data) return;
     const attr:ProductCustom = data.product;
 
